569e5d95087759a5c823ab73bac3cf72941d73a4
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-03-24 10:19:43 -0300

    Add files via upload
    Exercícios do professor Messias (Introdução a Programação)

---
+++ questoes_messias.py
@@ -0,0 +1,50 @@
+#Questão 1
+nome = input("Digite seu nome: ")
+print(f"Eu,{nome},amo programar em Python")
+
+#Questão 2
+
+nome = input("Digite seu nome: ")
+print(f"Bom dia,Sr.{nome},tenha um ótimo dia de trabalho.")
+#Questão 3
+
+nome_u = int(input("Crie um número de usuário: "))
+print(f"Seu número é: {nome_u}")
+
+soma = (nome_u + 1357)
+print(f"O resultado da soma é: {soma}")
+soma2 = (soma * 8)
+print(f"O resultado da multiplicação é: {soma2}")
+soma3 = (soma2 / 5)
+print(f"O resultado da divisão é: {soma3}")
+soma4 = (soma3 **4)
+print(f"O resultado da elevação é: {soma4}")
+
+#Questão 4
+
+nome1 = int(input("Crie um número de usuário: "))
+print(f"Seu número é: {nome1}")
+soma = (nome1 + 7)
+print(f"O resultado da soma é: {soma}")
+
+
+nome2 = int(input("Crie um número de usuário: "))
+print(f"Seu número é: {nome2}")
+subtra = (nome2 - 8)
+print(f"O resultado da subtração é: {subtra}")
+
+
+nome3 = int(input("Crie um número de usuário: "))
+print(f"Seu número é:{nome3})")
+multi = (nome3 * 8)
+print(f"O resultado da multiplicação é: {multi}")
+
+nome4 = int(input("Crie um número de usuário: "))
+print(f"Seu número é:{nome4})")
+divisao = (nome4 / 2)
+print(f"O resultado da divisão é: {divisao}")
+
+nome5 = float(input("Crie um número de usuário: "))
+print(f"Seu número é:{nome5})")
+elev = (nome5 **1)
+print(f"O resultado da elevação é: `{elev}")
