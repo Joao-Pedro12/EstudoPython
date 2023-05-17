4d0e06893afb92d125c2a077609ca155e8e455ad
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-05-17 19:35:19 -0300

    Add files via upload

---
+++ TED1112/filmes01.py
@@ -0,0 +1,23 @@
+import csv
+
+
+with open ('filmes.csv', 'r', encoding='utf-8')as arquivo:
+    filme_visto = csv.reader(arquivo, delimiter=',')
+    
+    vizu = 2
+    maisvisto = ""
+
+    for linha in filme_visto:
+        vizumax = linha[2]
+        filmemax = int(linha[8])
+    if int(vizumax) > vizu:
+        vizu = int(vizumax)
+        maisvisto = int(linha[8])
+        print("O filme mais visto é:", filmemaisvisto)
+        print("Número de visualizações:", vizu)
+
+
+
+            
+        
+        
