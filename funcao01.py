1a0991a75e321701d7f426376ef8770da37cd1c5
João Pedro Silva Souza Nunes <126632669+Joao-Pedro12@users.noreply.github.com>
2023-05-22 20:48:04 -0300

    Add files via upload

---
+++ funcao01.py
@@ -0,0 +1,13 @@
+def soma(num1,num2):
+    #O sinal muda toda a função
+    resultado= num1 + num2
+    return(resultado)
+
+#print(soma(4,10))
+#print(soma(int(input('Digite um num1:')),int(input('Digite o num2:'))))
+#print(soma(3,5))
+#print(soma(10,5))
+#print(soma(3,6789))
+
+resultado_1 = soma(3,5)
+print(f'O resiltado do primeiro é igual a:{resultado_1}')
